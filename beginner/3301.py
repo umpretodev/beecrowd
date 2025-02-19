eaf4dfb9e9287b579ab45a6755c5ecd8d0f225d9
Pedro Fernandes <lebackman18@gmail.com>
2025-02-18 22:29:04 -0300

    🚧wip: beginner/3301

---
+++ beginner/3301.py
@@ -0,0 +1,11 @@
+cousin_years = [] 
+
+for i in range(3):
+    year = int(input())
+    
+    if i == 0: cousin_years.append()
+    if i == 1: cousin_years.append()
+    if i == 2: cousin_years.append()
+
+cousin_years.sort()
+
