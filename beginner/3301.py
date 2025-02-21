e9464762967a8e61dbcba1f04840f42ad4d55160
Lebackrobot <lebackman18@gmail.com>
2025-02-20 23:02:17 -0300

    ✨ feat: beginner/3301

--- beginner/3301.py
+++ beginner/3301.py
@@ -1,11 +1,17 @@
-cousin_years = [] 
+cousins = [] 
 
-for i in range(3):
-    year = int(input())
-    
-    if i == 0: cousin_years.append()
-    if i == 1: cousin_years.append()
-    if i == 2: cousin_years.append()
+years = str(input()).split()
 
-cousin_years.sort()
+for i, year in enumerate(years):
+    name = ''
 
+    if i == 0: name = 'huguinho'
+    if i == 1: name = 'zezinho'
+    if i == 2: name = 'luisinho'
+
+    cousins.append({ 'name': name, 'years': year })
+
+sorted_cousins = sorted(cousins, key=lambda dict: dict['years'])
+middle_cousin = sorted_cousins[1]
+
+print(middle_cousin.get('name'))
