98b391cd9789dcfa76e02dd4879e21676ab76a2b
Lebackrobot <lebackman18@gmail.com>
2025-03-04 13:06:44 -0300

    🚧 wip: beginner/1213

--- beginner/1213.cpp
+++ beginner/1213.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
+#include <string> 
+
 using namespace std; 
+
+int handleResponse(int number) {
+    string multipleOnes = "11"; 
+
+    while (stoi(multipleOnes) % number != 0) {
+        multipleOnes += '1';
+    }
+
+    return  multipleOnes.size();
+}
 
 int main() {
     int number;
 
-    while(cin.eof()) {
-        cin >> number;
-
-        for (int i = 0; i<number; i++) {
-
+    while(true) {
+            cin >> number;
+            cout << handleResponse(number) << endl;
         }
-    }
-
-
     return 0;
 }
